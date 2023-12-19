49bc818c3cda3cb5213b6eb856b795e181ebc022
waldirjr79 <wjrgol83@gmail.com>
2023-12-18 21:05:58 -0300

    atuCasa

--- static/js/contador.js
+++ static/js/contador.js
@@ -24,9 +24,28 @@ btnlogin.addEventListener('click', () => {
 
 codbarra = document.querySelector('#codbarra')
 
+function addLinhaTab(){
+    var tabela = document.querySelector("#tabela-contagem")
+    var linha = tabela.insertRow(1)
+    var c0 = linha.insertCell(0)
+    var c1 = linha.insertCell(1)
+    var c2 = linha.insertCell(2)
+
+    linha.style.backgroundColor = '#141414'
+    linha.style.color = 'white'
+    linha.style.heigth = '15px'
+
+    c0.innerHTML = 1
+    c1.innerHTML = codbarra.value
+    c2.innerHTML = 1
+    console.log(codbarra.value)
+}
+
 codbarra.addEventListener('keydown', function(event) {
     if (event.key === "Enter") {
-        alert('Capturado')
-        codbarra.value = ''
+        if (codbarra.value){
+            addLinhaTab()
+            codbarra.value = ''
+        }
     }
 })
